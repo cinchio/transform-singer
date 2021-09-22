f70972b83f00ecbf181ed232932763a62701526e
Chris London <me@chrislondon.co>
2021-09-21 20:11:28 -0600

    Adding in conditional transform type

--- transform_singer/processor.py
+++ transform_singer/processor.py
@@ -15,8 +15,47 @@ class Processor:
                 # Convert this schema
                 pass
 
+    def _process_condition(self, obj, record):
+        """
+        Example:
+        Compares the record's first_name with the text value of "Chris"
+        {
+            "operator": "eq",
+            "left": {
+                "type": "record",
+                "key": "first_name",
+            },
+            "right": {
+                "type": "text",
+                "val": "Chris"
+            }
+        }
+        """
+        if obj['operator'] == 'and':
+            return all(self._process_condition(condition, record) for condition in obj['conditions'])
+        if obj['operator'] == 'or':
+            return any(self._process_condition(condition, record) for condition in obj['conditions'])
+
+        left = self.process_mapping(obj.get('left'), record)
+        right = self.process_mapping(obj.get('right'), record)
+
+        if obj['operator'] == 'eq':
+            return left == right
+        if obj['operator'] == 'lt':
+            return left < right
+        if obj['operator'] == 'lte':
+            return left <= right
+        if obj['operator'] == 'gt':
+            return left > right
+        if obj['operator'] == 'gte':
+            return left >= right
+
+        return False
+
     def process_mapping(self, mapping, record):
-        if mapping['type'] == 'record':
+        if not mapping:
+            return
+        elif mapping['type'] == 'record':
             # Grab a potentially nested value from the record object
             return nested_get(record, mapping['key'])
         elif mapping['type'] == 'config':
@@ -31,8 +70,49 @@ class Processor:
         elif mapping['type'] == 'coalesce':
             # Find the first processed value this exists and use that.
             return next((self.process_mapping(mapping, record) for mapping in mapping['objects'] if self.process_mapping(mapping, record)), None)
+        elif mapping['type'] == 'if':
+            """
+            Run a condition on the mapping.   This particularly useless mapping will change the first_name if it's Chris to Christopher.
+            Otherwise it will return the original first_name.  For nested if statements you can add another "if" type in the "else" or the
+            "then".
+
+            Example:
+            {
+                "type": "if",
+                "condition": {
+                    "operator": "eq",
+                    "left": {
+                        "type": "record",
+                        "key": "first_name",
+                    },
+                    "right": {
+                        "type": "text",
+                        "val": "Chris"
+                    }
+                }
+                "then": {
+                    "type": "text",
+                    "val": "Christopher"
+                },
+                "else": {
+                    "type": "record",
+                    "key": "first_name",
+                }
+            }
+            """
+            if self._process_condition(mapping.get('condition'), record):
+                # If the condition passed, then return the "then" value
+                return self.process_mapping(mapping.get('then'), record)
+
+            # Condition didn't pass, send the "else" value
+            return self.process_mapping(mapping.get('else'), record)
 
     def process_record(self, message):
+        if message['stream'] not in self.config['mappings']:
+            # Do we want to pass no the record?
+            # singer.write_record(message['stream'], message['record'])
+            return
+
         for mapping in self.config['mappings'][message['stream']]:
             # Loop through the mappings of this stream and process the record.
             record = {}
